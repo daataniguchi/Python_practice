313c5b23eb0d79f8f2a6d524eec70f2e4ae6c256
Anissa Garcia <anissagarcia@Anissas-MacBook-Air.local>
2020-03-20 16:39:14 -0700

    duplicate practice

---
+++ duplicate_practice.py
@@ -0,0 +1,30 @@
+a = [1,2,2,3,4,4,5,6,17]
+b = [1,2,2,3,4,5,6,6,7,10,11]
+
+import collections
+print([item for item, count in collections.Counter(a).items()])
+print([item for item, count in collections.Counter(b).items()])
+
+
+seen = set()
+uniq_a = [x for x in a if x not in seen and not seen.add(x)]
+uniq_b = [x for x in b if x not in seen and not seen.add(x)]
+
+print (uniq_a)
+print (uniq_b)
+
+
+
+seen = {}
+dupes = []
+
+for x in a:
+    if x not in seen:
+        seen[x] = 1
+    else:
+        if seen[x] == 1:
+            dupes.append(x)
+        seen[x] +=1
+        
+print(seen)
+print(dupes)
