0bc04f04ac12a010e0fa29ced361b9593d1c1020
Keomony Diep <diep003@cougars.csusm.edu>
2020-04-04 18:40:02 -0700

    Update extract_files_from_folder.py

--- extract_files_from_folder.py
+++ extract_files_from_folder.py
@@ -6,7 +6,7 @@ Type "help", "copyright", "credits" or "license()" for more information.
 def func_2():
     path = '/Users/keomonydiep/Desktop/training_images/ciliates/'##path to the ciliate image files
     a = os.listdir(path) ##retrieve image files using os.listdir()
-    print(a[:4]) ##print out the first two images within the list
+    print(a[:4]) ##print out the first four images within the list
     print('length of images in ciliates is ' + str(len(a))) ##print out the length of files in the ciliates
 
     path_2 = '/Users/keomonydiep/Desktop/training_images/l_poly/' ##path to l_poly image files
@@ -16,7 +16,7 @@ def func_2():
 
     path_3 = '/Users/keomonydiep/Desktop/training_images/other/' ##path to other image files
     c = os.listdir(path_3) ##retrieve image files
-    print(c[:4]) ##print out the first eight images within the list
+    print(c[:4]) ##print out the first four images within the list
     print('length of images in other is ' + str(len(c))) #print out length of files in other
 
 func_2()
