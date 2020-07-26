0b79dcc682686e557eac70cf8be010d38eff5bef
Anissa Garcia <garci687@cougars.csusm.edu>
2020-07-25 22:50:22 -0700

    counts the number of files

---
+++ Python_practice/num_files.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 22:46:10 2020
+
+@author: anissagarcia
+"""
+
+import os
+
+list = os.listdir("folder/directory")
+number_files = len(list)
+
+print (number_files)
