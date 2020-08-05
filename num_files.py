499be1a6a9b1d8eb340867938e17849bd6ff37cb
Anissa Garcia <garci687@cougars.csusm.edu>
2020-08-05 04:54:12 -0700

    updated with the number of files given

--- num_files.py
+++ num_files.py
@@ -7,31 +7,25 @@ Created on Sat Jul 25 22:46:10 2020
 """
 
 import os
-##################################################
-####### Below will give you the file count #######
-##################################################
+from pathlib import Path
+        
+path = "/dir/path/"
+categories = ["#insert catergory name(s)"]
 
-#list = os.listdir("folder/directory")
-#number_files = len(list)
+file_list = []
+file_num = []
 
-#print (number_files)
-
-#################################################
-##### Below willl give you a list of the folder and the subdirectories######
-#################################################
-
-import os
-path = "/Users/anissagarcia/Desktop/Research_2020/Anissa_classified_images/SPCP2_unlabeled_data_part_3"
-categories = ['Ciliate', 'L.poly', 'Questionable', 'Other']
-#subdirs = [x[0] for x in os.walk(path)]
-
-#files = []
 for r, d, f in os.walk(path):
+    if r.split('/'[-1]) == categories:
+        num_files = len(f)
+        file_list.append(f)
+        file_num.append(num_files)
+        num_files.append(r, d, f)
+        file_count = sum(len(f) for _, _, files in os.walk(path))
+        
     print('r', r)
     print('d', d)
     print('f', f)
-#    for file in f:
- #       files.append(os.path.join(r, file))
-#for f in files:
- #   print(f)
-#print(subdirs)
+    print('Total: ', len(f))
+
+print('Overall Total: ', file_count-1) #-1 is added to exclude the hidden folder
