25d0c431cbfbc4c7e8beb23497b748c1829dbeb6
unknown <lgut32@gmail.com>
2022-02-14 12:29:47 -0800

    Work in progress, needs polishing

--- Growth Rates.py
+++ Growth Rates.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-def mean_data(file, sheet, group, cols): #avg refers to the variable which will be averaged
+def mean_data(file, sheet, group, cols):
     data = pd.read_excel(file , sheet_name=sheet, usecols= cols)
     drop_0 = data.drop([0,1,2])
     print(drop_0)
@@ -10,21 +10,21 @@ def mean_data(file, sheet, group, cols): #avg refers to the variable which will 
     df = drop.groupby(group, as_index=False, dropna=True).mean()
     return df
 
-def chl_0(file, sheet):
+def chl_0(file, sheet): #chl value used as chl0
     data = pd.read_excel(file, sheet_name=sheet)
     value = data['Chlorophyll, ug/L'].values[0]
     return value
 
 
-x = chl_0('C:\\Users\\Luis\\Research\\Chlorophyll\\Chlorophyll_Data.xlsx', 'Experiment_4')
+x = chl_0('C:\\Users\\Luis\\Research\\Chlorophyll\\Chlorophyll_Data.xlsx', 'Experiment_4') #grabbing chl0 value
 
 avg_data = mean_data('C:\\Users\\Luis\\Research\\Chlorophyll\\Chlorophyll_Data.xlsx','Experiment_4', 'Bottle_number', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'])
 print(avg_data)
-avg_data['Apparent growth rate day'] = 2*np.log((avg_data['Chlorophyll, ug/L']/(x*avg_data['Fraction_whole_seawater'])))
+avg_data['Apparent growth rate day'] = 2*np.log((avg_data['Chlorophyll, ug/L']/(x*avg_data['Fraction_whole_seawater']))) #calculating day growth rates
 
 
 
-plt.scatter(avg_data['Fraction_whole_seawater'], avg_data['Apparent growth rate day'], )
+plt.scatter(avg_data['Fraction_whole_seawater'], avg_data['Apparent growth rate day'], ) #plotting growth rates
 plt.show()
 
 
