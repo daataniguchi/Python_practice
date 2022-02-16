60255269ac1c866e35a6d05a0d5428c822301b14
unknown <lgut32@gmail.com>
2022-02-16 11:23:20 -0800

    Added comments, needs polishing

--- Growth Rates.py
+++ Growth Rates.py
@@ -1,37 +1,33 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-def mean_data_chl_1(file, sheet, group, cols):
-    data = pd.read_excel(file , sheet_name=sheet, usecols= cols)
-    drop = data.drop([0,1,2]).replace(0, np.nan).dropna(axis=1, how='all')
-    df = drop.loc[drop['Time_point'] == 1].groupby(group, as_index=False, dropna=True, sort=False).mean()
+def mean_data_chl_1(file, sheet, group, cols): #creates chl_1 values
+    data = pd.read_excel(file , sheet_name=sheet, usecols= cols) #reads data
+    drop = data.drop([0,1,2]).replace(0, np.nan).dropna(axis=1, how='all') #drops chl_0 rows and replaces any 0 values with NA then drops them
+    df = drop.loc[drop['Time_point'] == 1].groupby(group, as_index=False, dropna=True, sort=False).mean() #selects only rows with time point = 1, groups by bottle number and takes the average
     return df
 
-def mean_data_chl_2(file, sheet, group, cols):
-    data = pd.read_excel(file , sheet_name=sheet, usecols= cols)
-    drop = data.drop([0,1,2]).replace(0, np.nan).dropna(axis=1, how='all')
-    df = drop.loc[drop['Time_point'] == 2].groupby(group, as_index=False, dropna=True, sort=False).mean()
+def mean_data_chl_2(file, sheet, group, cols): #creates chl_2 values
+    data = pd.read_excel(file , sheet_name=sheet, usecols= cols) #reads data
+    drop = data.drop([0,1,2]).replace(0, np.nan).dropna(axis=1, how='all') #drops chl_0 rows and replaces any 0 values with NA then drops them
+    df = drop.loc[drop['Time_point'] == 2].groupby(group, as_index=False, dropna=True, sort=False).mean() #selects only rows with time point = 2, groups by bottle number and takes the average
     return df
 
-def chl_0(file, sheet, cols): #chl value used as chl0
-    data = pd.read_excel(file, sheet_name=sheet, usecols= cols)
-    data['Chlorophyll, ug/L'] = data['Chlorophyll, ug/L'].replace(0, np.nan)
-    value = data.loc[data['Time_point'] == 0].mean()
-    chl_0 = value['Chlorophyll, ug/L']
+def chl_0(file, sheet, cols): #chl value used as chl_0
+    data = pd.read_excel(file, sheet_name=sheet, usecols= cols) #reads data
+    data['Chlorophyll, ug/L'] = data['Chlorophyll, ug/L'].replace(0, np.nan) #replaces any chl = 0 values with NA
+    value = data.loc[data['Time_point'] == 0].mean() #selects only rows with time point 0 and takes the average
+    chl_0 = value['Chlorophyll, ug/L'] #locates chl_0 value
     return chl_0
 
-
 x = chl_0('C:\\Users\\Luis\\Research\\Chlorophyll\\Chlorophyll_Data.xlsx', 'Experiment_4', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater']) #grabbing chl0 value
-print(x)
 avg_data_chl_1 = mean_data_chl_1('C:\\Users\\Luis\\Research\\Chlorophyll\\Chlorophyll_Data.xlsx','Experiment_4', 'Bottle_number', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'])
-print(avg_data_chl_1)
 avg_data_chl_2 = mean_data_chl_2('C:\\Users\\Luis\\Research\\Chlorophyll\\Chlorophyll_Data.xlsx','Experiment_4', 'Bottle_number', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'])
-print(avg_data_chl_2)
 
 
 avg_data_chl_1['Apparent growth rate day'] = 2*np.log((avg_data_chl_1['Chlorophyll, ug/L']/(x*avg_data_chl_1['Fraction_whole_seawater']))) #calculating day growth rates
-avg_data_chl_1['Apparent growth rate night'] = 2*np.log(avg_data_chl_2['Chlorophyll, ug/L']/avg_data_chl_1['Chlorophyll, ug/L'])
-avg_data_chl_1['Apparent growth rate 24 hrs'] = 1*np.log((avg_data_chl_2['Chlorophyll, ug/L']/(x*avg_data_chl_2['Fraction_whole_seawater'])))
+avg_data_chl_1['Apparent growth rate night'] = 2*np.log(avg_data_chl_2['Chlorophyll, ug/L']/avg_data_chl_1['Chlorophyll, ug/L']) #calculates night growing rates
+avg_data_chl_1['Apparent growth rate 24 hrs'] = 1*np.log((avg_data_chl_2['Chlorophyll, ug/L']/(x*avg_data_chl_2['Fraction_whole_seawater']))) #calculating 24 hrs growth rates
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(avg_data_chl_1)
 
