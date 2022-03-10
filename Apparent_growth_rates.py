56aeba90da38d770e29d2a9d01395878a0558a02
unknown <lgut32@gmail.com>
2022-03-09 17:10:51 -0800

    Measures apparents growth rates based on chlorohpyll values

--- Apparent_growth_rates.py
+++ Apparent_growth_rates.py
@@ -37,21 +37,21 @@ def app_rates(x, y):
     print('Grazing rate =', -m) #prints slope aka grazing rate
 
 file = 'C:\\Users\\Luis\\Research\\Chlorophyll\\Chlorophyll_Data.xlsx'
-expetiment = 'Experiment_4'#select experiment to work on
+experiment = 'Experiment_8'#select experiment to work on
 
 
 data = orig_data(file, experiment , 'Bottle_number', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'])
 chl_0 = chlorophyll_tp_0(file, experiment , ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'])#selects data with timepoint = 2 and resets index
 chl_1 = chl_data(file, experiment , 'Bottle_number', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'], 1)
 chl_2 = chl_data(file, experiment , 'Bottle_number', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'], 2)
-data['Apparent growth rate day'] = 2*np.log((chl_1['Chlorophyll, ug/L']/(chl_0*chl_1['Fraction_whole_seawater']))) #calculating day apparent growth rates
-data['Apparent growth rate night'] = 2*np.log(chl_2['Chlorophyll, ug/L']/chl_1['Chlorophyll, ug/L']) #calculating night apparent growth rates
-data['Apparent growth rate 24 hrs'] = 1*np.log((chl_2['Chlorophyll, ug/L']/(chl_0*chl_2['Fraction_whole_seawater']))) #calculating 24 hr apparent growth rates
+data['Apparent_growth_rate_day'] = 2*np.log((chl_1['Chlorophyll, ug/L']/(chl_0*chl_1['Fraction_whole_seawater']))) #calculating day apparent growth rates
+data['Apparent_growth_rate_night'] = 2*np.log(chl_2['Chlorophyll, ug/L']/chl_1['Chlorophyll, ug/L']) #calculating night apparent growth rates
+data['Apparent_growth_rate_24_hrs'] = 1*np.log((chl_2['Chlorophyll, ug/L']/(chl_0*chl_2['Fraction_whole_seawater']))) #calculating 24 hr apparent growth rates
 
 final_data = data.dropna() #drops any NaN in the dataframe
 
-time_rate = final_data['Apparent growth rate day'] #selects apparent growth rate to work on
+time_rate = final_data['Apparent_growth_rate_24_hrs'] #selects apparent growth rate to work on
 error = np.std(time_rate)/np.sqrt(len(time_rate)) #creates error bars
 
-app_rates(final_data['Fraction_whole_seawater'], time_rate) #creates graph
+graph_rates(final_data['Fraction_whole_seawater'], time_rate) #creates graph
 
