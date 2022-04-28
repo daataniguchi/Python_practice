d793dae30578418ac972760961bf8eb1f555e87b
unknown <lgut32@gmail.com>
2022-04-27 17:16:35 -0700

    Needs slight clean up, maybe some modification

--- growth_and_grazing_rates.py
+++ growth_and_grazing_rates.py
@@ -9,20 +9,13 @@ def orig_data(file, sheet, group, cols):
     df = drop[cols].groupby(group, as_index=False, dropna=True, sort=False).mean()
     return df
 
-def chlorophyll_tp_0(file, sheet, cols): #chl value used as chl0
-    data = pd.read_excel(file, sheet_name=sheet, usecols= cols) #reads data
-    data['Chlorophyll, ug/L'] = data['Chlorophyll, ug/L'].replace(0, np.nan) #specified chlorophyll column and raplaces any 0 with NaN
-    value = data.loc[data['Time_point'] == 0].mean() #identifies rows with timepoint = 0 and averages them
-    chl_0 = value['Chlorophyll, ug/L'] #creates chl_0 variable
-    return chl_0
-
 def chl_data(file, sheet, group, cols, timepoint): #creates dataframe to work with
     data = pd.read_excel(file , sheet_name=sheet, usecols= cols) #read data
     drop = data.dropna(axis=1, how='all') #drops first 3 columns, converts any 0 into NaN and drops them
     df = drop.loc[drop['Time_point'] == timepoint].groupby(group, as_index=False, dropna=True, sort=False).mean() #groups by variable and averages it
     return df
 
-def graph_rates(x, y): #create rates scatter plot
+def graph_apparent_rates(x, y): #create rates scatter plot
     m, b = np.polyfit(x, y , 1) #creates variable for line of best fit
     plt.scatter(x, y) #plots x, y values
     plt.plot(x, m*x+b) #plots line of best fit
@@ -33,9 +26,22 @@ def graph_rates(x, y): #create rates scatter plot
     print('Grazing rate =' , -m) #prints slope aka grazing rate
     plt.show()
 
-def graph_values(data):
+def graph_actual_rates(data):
     df = pd.DataFrame.from_dict({(i, j): data[i][j] for i in data.keys() for j in data[i].keys()}, orient='index').reset_index().rename(columns={'level_0': 'Location', 'level_1': 'Experiment'}).melt(['Location', 'Start_time', 'Experiment']).rename(columns={'variable': 'Rate'})
-    sns.scatterplot(x='Location', y='value', data=df, marker='.', style='Start_time', hue='Rate', sort= False)
+    df['Location'] = pd.Categorical(df['Location'], categories=['Offshore', 'Shelf_break', 'Mid_shelf', 'Nearshore'], ordered=True)
+    plt.figure(figsize=(11, 8))
+    sns.set_style('dark')
+    sns.set_context('talk')
+    sns.scatterplot(x='Location', y='value', data=df, style='Rate', size='Start_time', hue='Rate')
+    if data == growth:
+        plt.ylabel('Growth Rate (1/Day)')
+        plt.ylim((-0.30, 1.90))
+    elif data == grazing:
+        plt.ylabel('Grazing Mortality Rate (1/Day')
+        plt.ylim((-0.30, 1.90))
+    elif data == net:
+        plt.ylabel('Net Growth Rate (1/Day)')
+    plt.show()
 
 
 file = 'C:\\Users\\Luis\\Research\\Chlorophyll\\Chlorophyll_Data.xlsx'
@@ -49,16 +55,17 @@ net = {}
 r2_value = {}
 e_even = []
 e_odd = []
+st_error = []
 for e in experiment:
     if e == 'Profiles':
         continue
     data = orig_data(file, e, ['Station_description', 'Bottle_number'], ['Sampling_start_time', 'Station_description', 'Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'])
-    chl_0 = chlorophyll_tp_0(file, e, ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'])  # selects data with timepoint = 2 and resets index
-    chl_1 = chl_data(file, e, 'Bottle_number', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'], 1)
-    chl_2 = chl_data(file, e, 'Bottle_number', ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'], 2)
-    data['Day_rate'] = 2 * np.log((chl_1['Chlorophyll, ug/L'] / (chl_0 * chl_1['Fraction_whole_seawater'])))  # calculating day apparent growth rates
+    chl_0 = chl_data(file, e, ['Bottle_number', 'Time_point'], ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'], 0)
+    chl_1 = chl_data(file, e, ['Bottle_number', 'Time_point'], ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'], 1)
+    chl_2 = chl_data(file, e, ['Bottle_number', 'Time_point'], ['Chlorophyll, ug/L', 'Bottle_number', 'Time_point', 'Fraction_whole_seawater'], 2)
+    data['Day_rate'] = 2 * np.log((chl_1['Chlorophyll, ug/L'] / (chl_0['Chlorophyll, ug/L'].values[0] * chl_1['Fraction_whole_seawater'])))  # calculating day apparent growth rates
     data['Night_rate'] = 2 * np.log(chl_2['Chlorophyll, ug/L'] / chl_1['Chlorophyll, ug/L'])  # calculating night apparent growth rates
-    data['24_hrs_rate'] = 1 * np.log((chl_2['Chlorophyll, ug/L'] / (chl_0 * chl_2['Fraction_whole_seawater'])))  # calculating 24 hr apparent growth rates
+    data['24_hrs_rate'] = 1 * np.log((chl_2['Chlorophyll, ug/L'] / (chl_0['Chlorophyll, ug/L'].values[0] * chl_2['Fraction_whole_seawater']))) # calculating 24 hr apparent growth rates
     final_data = data.dropna()
     if final_data['Sampling_start_time'].iloc[0] > 1000:
         e_even.append(e)
@@ -96,6 +103,8 @@ for e in experiment:
         net[station][e].update({'Start_time': 'Nighttime'})
     for t in time_rate:
         m, b = np.polyfit(final_data['Fraction_whole_seawater'], final_data[t], 1)
+        error = np.std(final_data[t]) / np.sqrt(len(final_data[t]))
+        st_error.append(error)
         growth[station][e][t]= b
         grazing[station][e][t]= -m
         net[station][e][t] = b - (-m)
@@ -104,17 +113,13 @@ for e in experiment:
         R_sq = corr ** 2
         r2_value[station][e][t] = R_sq
 
-df = pd.DataFrame.from_dict({(i, j): growth[i][j] for i in growth.keys() for j in growth[i].keys()}, orient='index').reset_index().rename(columns={'level_0': 'Location', 'level_1': 'Experiment'}).melt(['Location', 'Start_time', 'Experiment']).rename(columns={'variable': 'Rate'})
-
-
+graph_actual_rates(growth)
+graph_actual_rates(grazing)
+graph_actual_rates(net)
+"""
 print('grazing rates:', grazing)
 print('growth rates:', growth)
 print('net rates:', net)
 print('r2 values are:', r2_value)
-
-
-fig1 = graph_values(growth)
-plt.show()
-fig2 = graph_values(grazing)
-plt.show()
+"""
 
